2cd2e95a5b505ba1672182cdf58840de522d9c7d
Trarco <trarco22@outlook.it>
2025-07-04 13:22:46 +0200

    Add IT language

--- lang/en/block_books_on_offer.php
+++ lang/en/block_books_on_offer.php
@@ -1,11 +1,26 @@
 <?php
-$string['pluginname'] = 'Books on Offer';
-$string['addinstance'] = 'Add a new Books on Offer block';
-$string['configtitle'] = 'Books on Offer configuration';
-$string['title'] = 'Block title';
-$string['subtitle'] = 'Block subtitle';
-$string['bookimage'] = 'Book image';
-$string['booktitle'] = 'Book title';
-$string['bookauthor'] = 'Book author';
-$string['deletebook'] = 'Delete this book';
-$string['markdelete'] = 'Mark for deletion';
+// English strings for block_books_on_offer
+// @package   block_books_on_offer
+// @copyright 2025 Trarco
+// @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname']       = 'Books on Offer';
+$string['addinstance']      = 'Add a Books on Offer block';
+$string['configtitle']      = 'Books on Offer settings';
+$string['title']            = 'Block title';
+$string['subtitle']         = 'Block subtitle';
+
+$string['bookimage']        = 'Book image';
+$string['booktitle']        = 'Book title';
+$string['bookauthor']       = 'Book author';
+
+$string['addmorebooks']     = 'Add more books';
+
+$string['deletebook']       = 'Delete this book';
+$string['markdelete']       = 'Mark for deletion';
+
+// Capability descriptions (optional)
+$string['books_on_offer:addinstance']   = 'Add a new Books on Offer block';
+$string['books_on_offer:myaddinstance'] = 'Add a Books on Offer block to My home';
---
+++ lang/it/block_books_on_offer.php
@@ -0,0 +1,26 @@
+<?php
+// Italian strings for block_books_on_offer
+// @package   block_books_on_offer
+// @copyright 2025 Trarco
+// @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname']               = 'Libri in Offerta';
+$string['addinstance']              = 'Aggiungi blocco Libri in Offerta';
+$string['configtitle']              = 'Impostazioni Libri in Offerta';
+$string['title']                    = 'Titolo blocco';
+$string['subtitle']                 = 'Sottotitolo blocco';
+
+$string['bookimage']                = 'Immagine del libro';
+$string['booktitle']                = 'Titolo del libro';
+$string['bookauthor']               = 'Autore del libro';
+
+$string['addmorebooks']             = 'Aggiungi altri libri';
+
+$string['deletebook']               = 'Elimina questo libro';
+$string['markdelete']               = 'Segna per eliminazione';
+
+// Descrizioni delle capability
+$string['books_on_offer:addinstance']   = 'Aggiungi un nuovo blocco Libri in Offerta';
+$string['books_on_offer:myaddinstance'] = 'Aggiungi un blocco Libri in Offerta alla mia home';
